77f7da51f44e4cf31cf5f4d8c418aab4837b9b32
Vincent Lagarde <lagardevincent64@gmail.com>
2022-03-15 18:26:04 +0100

    fin de la quete

---
+++ index.js
@@ -0,0 +1,10 @@
+
+var cowsay = require("cowsay");
+const student = require('./information');
+console.log(student)
+
+console.log(cowsay.say({
+    text : "i'm Vincent from  French wildCodeSchool ",
+    e : "oO",
+    T : "U "
+}));
---
+++ information.js
@@ -0,0 +1,9 @@
+const student = {name:'vincent', campus:'wildCodeSchool'};
+
+
+
+
+
+module.exports = {
+    student
+}
